745132f6ca6e1053c64d796cb5f684534923f069
Ekaterina7755 <Ekaterinalitvinova9156@gmail.com>
2025-01-16 22:27:54 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert("Пожалуйста, заполните все поля.");
+        event.preventDefault();
+    }
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Введите правильный адрес электронной почты.");
+        event.preventDefault();
+    }
+});
